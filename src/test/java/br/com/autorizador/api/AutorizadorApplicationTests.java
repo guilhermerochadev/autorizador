775efaf8c52909a4a6724fdbad893838cb48fad0
Guilherme Rocha <guilhermerochadev@gmail.com>
2023-04-22 17:15:23 -0300

    Criação do projeto

---
+++ src/test/java/br/com/autorizador/api/AutorizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.autorizador.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutorizadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
