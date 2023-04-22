4768681efad89731536a758ac6871ee3345d64f0
Guilherme Rocha <guilhermerochadev@gmail.com>
2023-04-22 18:35:17 -0300

    Criação da classe CartaoController e ajustes na estrutura do projeto

---
+++ src/main/java/br/com/autorizador/api/cartao/controller/CartaoController.java
@@ -0,0 +1,19 @@
+package br.com.autorizador.api.cartao.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Guilherme Rocha
+ * Criação da classe CartaoController
+ * Por padronização e boas práticas o mapeamento do RequestMapping foi versionado.
+ */
+@RestController
+@RequestMapping("/autorizador/cartao/api/v1/")
+public class CartaoController {
+
+
+   
+
+}
